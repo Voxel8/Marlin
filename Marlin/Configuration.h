55877978fbd05f381ce3e1372e037a0437e3073b
Steve Kelly <kd2cca@gmail.com>
2014-11-04 20:40:32 -0500

    new axis default lengths

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -331,11 +331,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 120
+#define X_MAX_POS 150
 #define X_MIN_POS 0
-#define Y_MAX_POS 60
+#define Y_MAX_POS 115
 #define Y_MIN_POS 0
-#define Z_MAX_POS 100
+#define Z_MAX_POS 80
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
