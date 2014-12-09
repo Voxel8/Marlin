8ac4457d2f960984a5b0f4556c07365ea89c1840
Jack Minardi <jack@minardi.org>
2014-12-09 16:13:47 -0500

    remove extruder offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -397,7 +397,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
-  #define X_PROBE_OFFSET_FROM_EXTRUDER 25
+  #define X_PROBE_OFFSET_FROM_EXTRUDER 0
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0
   #define Z_PROBE_OFFSET_FROM_EXTRUDER 0
 
