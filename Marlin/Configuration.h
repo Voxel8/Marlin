17528874a20b14365b273e1cab1da1ed81afd9d3
Jack Minardi <jack@minardi.org>
2015-03-28 19:46:10 -0400

    PID
    change PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -188,9 +188,9 @@ Here are some standard links for getting your machine calibrated:
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+    #define  DEFAULT_Kp 39.07
+    #define  DEFAULT_Ki 4.37
+    #define  DEFAULT_Kd 87.38
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
