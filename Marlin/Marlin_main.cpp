97bb5b5d7428f56542cf62b2ead4500ace4913b2
Ricky Rininger <pizzyflavin@gmail.com>
2015-04-08 16:31:53 -0400

    Add M-code M125 to set target pressure, modify M105 to show current_pressure / target_pressure.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -124,6 +124,7 @@
 // M115 - Capabilities string
 // M117 - display message
 // M119 - Output Endstop status to serial port
+// M125 - Set pneumatics target pressure
 // M126 - Solenoid Air Valve Open (BariCUDA support by jmil)
 // M127 - Solenoid Air Valve Closed (BariCUDA vent to atmospheric pressure by jmil)
 // M128 - EtoP Open (BariCUDA EtoP = electricity to air pressure transducer by jmil)
@@ -2475,6 +2476,11 @@ Sigma_Exit:
 #endif
       setWatch();
       break;
+    case 125: // M125 - Set pneumatics pressure
+      if (code_seen('S')) {
+        setTargetPressure(code_value());
+      }
+      break;
     case 112: //  M112 -Emergency Stop
       kill();
       break;
@@ -2507,6 +2513,13 @@ Sigma_Exit:
       #else
         SERIAL_ERROR_START;
         SERIAL_ERRORLNPGM(MSG_ERR_NO_THERMISTORS);
+      #endif
+
+      #ifdef PNEUMATICS
+        SERIAL_PROTOCOLPGM(" P:");
+        SERIAL_PROTOCOL_F(pressurePneumatic(), 1);
+        SERIAL_PROTOCOLPGM(" /");
+        SERIAL_PROTOCOL_F(targetPneumatic(), 1);
       #endif
 
         SERIAL_PROTOCOLPGM(" @:");
@@ -4706,7 +4719,6 @@ bool setTargetedHotend(int code){
   }
   return false;
 }
-
 
 float calculate_volumetric_multiplier(float diameter) {
   if (!volumetric_enabled || diameter == 0) return 1.0;
