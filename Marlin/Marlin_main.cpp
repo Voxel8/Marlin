4750fed474490f8bfa1039ac6e80cec143c5470d
Ricky Rininger <pizzyflavin@gmail.com>
2015-04-14 16:47:06 -0400

    Fix bug that causes problems when EXT_ADC is not defined.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3222,15 +3222,17 @@ Sigma_Exit:
       }
     }
     break;
-  case 234: // M234 - Return raw ADC value
-      SERIAL_PROTOCOLLN(EXT_ADC_RAW_0);
-    break;
-  case 235: // M235 - Report laser distance sensor reading
-      //SERIAL_PROTOCOLLN(MSG_M235_REPORT);         Commented out so only number is returned
-      //SERIAL_PROTOCOLPGM(MSG_EXT_ADC_REPORT);
-      SERIAL_PROTOCOLLN(EXT_ADC_READ_0);
-    break;
 
+  #ifdef EXT_ADC
+    case 234: // M234 - Return raw ADC value
+        SERIAL_PROTOCOLLN(EXT_ADC_RAW_0);
+      break;
+    case 235: // M235 - Report laser distance sensor reading
+        //SERIAL_PROTOCOLLN(MSG_M235_REPORT);         Commented out so only number is returned
+        //SERIAL_PROTOCOLPGM(MSG_EXT_ADC_REPORT);
+        SERIAL_PROTOCOLLN(EXT_ADC_READ_0);
+      break;
+  #endif
     #if NUM_SERVOS > 0
     case 280: // M280 - set servo position absolute. P: servo index, S: angle or microseconds
       {
