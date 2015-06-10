a4fd440b8c82712992d62ee7d3a01a35c9669a4b
Jack Minardi <jack@minardi.org>
2015-06-10 16:40:26 -0400

    define number of offsets

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -250,11 +250,7 @@ float zprobe_zoffset;
 
 // Extruder offset
 #if EXTRUDERS > 1
-#ifndef DUAL_X_CARRIAGE
-  #define NUM_EXTRUDER_OFFSETS 2 // only in XY plane
-#else
   #define NUM_EXTRUDER_OFFSETS 3 // supports offsets in XYZ plane
-#endif
 float extruder_offset[NUM_EXTRUDER_OFFSETS][EXTRUDERS] = {
   #if defined(EXTRUDER_OFFSET_X)
     EXTRUDER_OFFSET_X
