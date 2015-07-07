6afad231d122b968c4e6b40001783ed6c1912884
Travis Busbee <travis@voxel8.co>
2015-07-07 17:39:46 -0400

    Revert "define number of offsets"
    This reverts commit a4fd440b8c82712992d62ee7d3a01a35c9669a4b.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -250,7 +250,11 @@ float zprobe_zoffset;
 
 // Extruder offset
 #if EXTRUDERS > 1
+#ifndef DUAL_X_CARRIAGE
+  #define NUM_EXTRUDER_OFFSETS 2 // only in XY plane
+#else
   #define NUM_EXTRUDER_OFFSETS 3 // supports offsets in XYZ plane
+#endif
 float extruder_offset[NUM_EXTRUDER_OFFSETS][EXTRUDERS] = {
   #if defined(EXTRUDER_OFFSET_X)
     EXTRUDER_OFFSET_X
