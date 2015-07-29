6e84bcb9e5256e7e0662c02fb691fb1dbce8c5ef
Travis Busbee <travis@voxel8.co>
2015-07-29 14:41:59 -0400

    disabled software endstops
    disabled software endstops so the printer lets us jog without homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -470,8 +470,8 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
-#define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
-#define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
+#define min_software_endstops false // If true, axis won't move to coordinates less than HOME_POS.
+#define max_software_endstops false  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // @section machine
 
