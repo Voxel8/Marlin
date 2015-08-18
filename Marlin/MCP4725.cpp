dd2312bf23cbab9bb25d5386be28aa14c55f40ea
Richard Rininger <riningerr@wit.edu>
2015-08-17 22:53:44 -0400

    Fix I2C address of DAC and fix bit shifting of 12-bit data in cpp file.

--- Marlin/MCP4725.cpp
+++ Marlin/MCP4725.cpp
@@ -1,6 +1,8 @@
 #include "Configuration.h"
 #include "MCP4725.h"
-#include "Wire.h"
+
+#include "Arduino.h"
+#include <Wire.h>
 
 /*================================================================================*/
 /* DAC Write Function */
@@ -10,8 +12,8 @@ void DAC_write(uint8_t address, uint16_t data_val) {
 	// Normal 
 	uint8_t	config = (MCP4725_WRITE_DAC_ONLY | MCP4725_NORMAL_MODE);
 
-	uint8_t byte_a = data_val >> 8;
-	uint8_t byte_b = data_val & 0xFF;
+	uint8_t byte_a = data_val >> 4;
+	uint8_t byte_b = (data_val & 0x0F) << 4;
 
 	Wire.beginTransmission(address);
 	Wire.write(config);
@@ -28,8 +30,8 @@ void DAC_write_EEPROM(uint8_t address, uint16_t data_val) {
 	uint8_t	config = (MCP4725_WRITE_DAC_EEPROM | MCP4725_NORMAL_MODE);
 
 	// Break data_val down into bytes
-	uint8_t byte_a = data_val >> 8;
-	uint8_t byte_b = data_val & 0xFF;
+	uint8_t byte_a = data_val >> 4;
+	uint8_t byte_b = (data_val & 0x0F) << 4;
 
 	// Send the data over I2C
 	Wire.beginTransmission(address);
@@ -44,7 +46,7 @@ void DAC_write_EEPROM(uint8_t address, uint16_t data_val) {
 /*================================================================================*/
 void DAC_read(uint8_t address) {
 	// Add read bit to address (LSB)
-	uint8_t address = (address | MCP4725_READ); 
+	uint8_t address_modified = (address | MCP4725_READ); 
 
 	// This function is not done! at all!
 	// Probably want to actually return a value...
