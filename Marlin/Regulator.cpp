9081e67ad6aac88f11680b95f04fb8e0b0c7943e
Richard Rininger <riningerr@wit.edu>
2015-08-18 17:42:28 -0400

    Add Regulator files for electro-pneumatic reg. This generalizes the DAC files.

---
+++ Marlin/Regulator.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************************
+*
+* Intended for use with MCP4725 12-bit DAC
+*
+***********************************************************************************/
+#include "Regulator.h"
+#include "MCP4725.h"
+
+// Set the output pressure (in psi) of the regulator.
+void setOutputPressure(uint8_t pressure) {
+	// 4095/(130.355 psi)
+	uint16_t digital_val = (uint16_t)(31.414215 * pressure);
+	// Write value to DAC
+	DAC_write(MCP4725_I2C_ADDRESS, digital_val);
+}
---
+++ Marlin/Regulator.h
@@ -0,0 +1,10 @@
+#ifndef REGULATOR_H
+#define REGULATOR_H
+
+#include "Arduino.h"
+/*================================================================================*/
+/* Function Prototypes */
+/*================================================================================*/
+void setOutputPressure(uint8_t pressure);
+
+#endif
