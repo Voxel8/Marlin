89bade5c74ad735e42c155c8469af32bc117717f
Richard Rininger <riningerr@wit.edu>
2015-09-08 19:46:49 -0400

    Add DistanceSensor.h and DistanceSensor.cpp because they were missing

---
+++ Marlin/DistanceSensor.h
@@ -0,0 +1,37 @@
+/***********************************************************************************
+
+For use with Laser Distance Sensor
+
+@author Ricky Rininger - 4 September 2015
+
+***********************************************************************************/
+#include <stdint.h>
+#include "Configuration.h"
+
+#ifndef DISTANCESENSOR_H
+#define DISTANCESENSOR_H
+
+#if EXT_ADC == 1
+
+#define LINEAR_OFFSET	5.757    // linear coefficient of offset that was determined with empirical data
+#define CONST_OFFSET    1.57     // constant term of offset
+#define CONV_FACTOR     0.000188    // (6.144V / 32767 bits) 
+
+#elif EXT_ADC == 2
+
+#define LINEAR_OFFSET  	0		 // Constants not determined for this model
+#define CONST_OFFSET 	0
+#define CONV_FACTOR     0.003001    // (6.144V / 2047 bits) 
+
+#endif
+
+#define VOLT_TO_DIST    2000        // (10000 um / 5V)
+
+/* Function Prototypes */
+/*=====================*/
+
+uint16_t get_dist_SingleEnded(uint8_t channel);
+
+uint16_t get_dist_Differential(uint8_t first_channel, uint8_t second_channel);
+
+#endif
