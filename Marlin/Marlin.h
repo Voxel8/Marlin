2098f089b39ddfe374b4d33dc08b99e92780ad72
Kevin Murphy <murphyk3@wit.edu>
2015-09-21 11:02:58 -0400

    Added dwell function based off G4 to be called from anywhere (M241)

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -111,6 +111,7 @@ FORCE_INLINE void serialprintPGM(const char *str) {
 void get_command();
 void process_commands();
 uint16_t gcode_M238(uint8_t power = 0);
+void gcode_M241(long num_milliseconds = NULL);
 
 void manage_inactivity(bool ignore_stepper_queue=false);
 
