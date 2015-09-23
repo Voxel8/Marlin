8fa4a2885993ed7ea7aa3562892df048e03f3cd2
Kevin Murphy <murphyk3@wit.edu>
2015-09-23 15:18:11 -0400

    Added ifdef around M238 related things

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -111,7 +111,9 @@ FORCE_INLINE void serialprintPGM(const char *str) {
 void get_command();
 
 void idle(); // the standard idle routine calls manage_inactivity(false)
-uint16_t gcode_M238(uint8_t power = 0);
+#ifdef EXT_ADC
+  uint16_t gcode_M238(uint8_t power = 0);
+#endif
 void gcode_M241(long num_milliseconds = NULL);
 
 void manage_inactivity(bool ignore_stepper_queue=false);
