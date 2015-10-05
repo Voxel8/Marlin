610f6fb0e8503aedb10644e9a22712fc98039c2d
Richard Rininger <riningerr@wit.edu>
2015-10-05 17:56:32 -0400

    Define HEATER_1_PIN as CART2_SIG1_PIN so heater1 can be used in second cartridge position.

--- Marlin/pins_VOXEL8_GEN3C.h
+++ Marlin/pins_VOXEL8_GEN3C.h
@@ -10,13 +10,13 @@
 
 // Servo support
 #ifdef NUM_SERVOS
-  #define SERVO0_PIN       22 //motor header MX1
+  #define SERVO0_PIN            22  //motor header MX1
   #if NUM_SERVOS > 1
-    #define SERVO1_PIN     23 //Motor header MX2
+    #define SERVO1_PIN          23  //Motor header MX2
     #if NUM_SERVOS > 2
-      #define SERVO2_PIN   24 //Motor header MX3
+      #define SERVO2_PIN        24  //Motor header MX3
       #if NUM_SERVOS > 3
-        #define SERVO2_PIN  9 //pwm header pin 5
+        #define SERVO2_PIN      9   //pwm header pin 5
       #endif
     #endif
   #endif
@@ -36,60 +36,63 @@
 #undef E1_MS1_PIN
 #undef E1_MS2_PIN
  
-#define X_STEP_PIN    37  // Actuate X stepper driver
-#define X_DIR_PIN     48  // X Stepper driver direction
-#define X_MIN_PIN     24  // X min limit switch
-#define X_MAX_PIN     76  // X max limit switch
-#define X_ENABLE_PIN  29  // Enable X stepper driver
-#define X_MS1_PIN     40  // X stepper driver microstep select 1
-#define X_MS2_PIN     41  // X stepper driver microstep select 2
+#define X_STEP_PIN              37  // Actuate X stepper driver
+#define X_DIR_PIN               48  // X Stepper driver direction
+#define X_MIN_PIN               24  // X min limit switch
+#define X_MAX_PIN               76  // X max limit switch
+#define X_ENABLE_PIN            29  // Enable X stepper driver
+#define X_MS1_PIN               40  // X stepper driver microstep select 1
+#define X_MS2_PIN               41  // X stepper driver microstep select 2
 
-#define Y_STEP_PIN    36  // Actuate Y stepper driver
-#define Y_DIR_PIN     49  // Y Stepper driver direction
-#define Y_MIN_PIN     23  // Y min limit switch
-#define Y_MAX_PIN     77  // Y max limit switch
-#define Y_ENABLE_PIN  28  // Enable Y stepper driver
-#define Y_MS1_PIN     69  // Y stepper driver microstep select 1
-#define Y_MS2_PIN     39  // Y stepper driver microstep select 2
+#define Y_STEP_PIN              36  // Actuate Y stepper driver
+#define Y_DIR_PIN               49  // Y Stepper driver direction
+#define Y_MIN_PIN               23  // Y min limit switch
+#define Y_MAX_PIN               77  // Y max limit switch
+#define Y_ENABLE_PIN            28  // Enable Y stepper driver
+#define Y_MS1_PIN               69  // Y stepper driver microstep select 1
+#define Y_MS2_PIN               39  // Y stepper driver microstep select 2
 
-#define Z_STEP_PIN    35  // Actuate Z stepper driver
-#define Z_DIR_PIN     47  // Z Stepper driver direction
-#define Z_MIN_PIN     73  // Z min limit switch
-#define Z_MAX_PIN     30  // Z max limit switch
-#define Z_ENABLE_PIN  27  // Enable Z stepper driver
-#define Z_MS1_PIN     68  // Z stepper driver microstep select 1
-#define Z_MS2_PIN     67  // Z stepper driver microstep select 2
+#define Z_STEP_PIN              35  // Actuate Z stepper driver
+#define Z_DIR_PIN               47  // Z Stepper driver direction
+#define Z_MIN_PIN               73  // Z min limit switch
+#define Z_MAX_PIN               30  // Z max limit switch
+#define Z_ENABLE_PIN            27  // Enable Z stepper driver
+#define Z_MS1_PIN               68  // Z stepper driver microstep select 1
+#define Z_MS2_PIN               67  // Z stepper driver microstep select 2
 
 /************************
     DIGITAL POTENTIOMETER PINS
 *************************/
 // Currently used for limiting motor current
 #undef DIGIPOTSS_PIN
-#define DIGIPOTSS_PIN 38              // Chip Select for digipot SPI communication
-#define DIGIPOT_CHANNELS {4,5,3,0,1}  // X Y Z E0 E1 digipot channels to stepper driver mapping
+#define DIGIPOTSS_PIN           38  // Chip Select for digipot SPI communication
+#define DIGIPOT_CHANNELS        {4,5,3,0,1}  // X Y Z E0 E1 digipot channels to stepper driver mapping
 
 /*************************
 	  HEATED BED PINS
 *************************/
-#define BED_AVAIL_PIN   82  // From key circuit
-#define HEATER_BED_PIN  4   // Drive the heated bed
-#define TEMP_BED_PIN    2   // Heated bed thermistor
+#define BED_AVAIL_PIN           82  // From key circuit
+#define HEATER_BED_PIN          4   // Drive the heated bed
+#define TEMP_BED_PIN            2   // Heated bed thermistor
 
 /*************************
 	  CARTRIDGE PINS
 *************************/
-#define CART1_SIG1_PIN  5   // PWM Capable
-#define CART1_SIG2_PIN  46  // PWM Capable
-#define CART1_SIG3_PIN  74  // 
-#define CART2_SIG1_PIN  45  // PWM Capable
-#define CART2_SIG2_PIN  75  // Interrupt Capable
-#define CART2_SIG3_PIN  85
+#define CART1_SIG1_PIN          5   // PWM Capable
+#define CART1_SIG2_PIN          46  // PWM Capable
+#define CART1_SIG3_PIN          74  // 
+#define CART2_SIG1_PIN          45  // PWM Capable
+#define CART2_SIG2_PIN          75  // Interrupt Capable
+#define CART2_SIG3_PIN          85
 
 /*************************
 	  FFF PINS
 *************************/
-#define HEATER_0_PIN  CART1_SIG1_PIN
-#define TEMP_0_PIN 6
+#define HEATER_0_PIN            CART1_SIG1_PIN
+#define TEMP_0_PIN              6   // A6 Input Cart0 Therm
+
+#define HEATER_1_PIN            CART2_SIG1_PIN
+#define TEMP_1_PIN              1   // A1 Input Cart1 Therm
 
 /*************************
 	  PNEUMATIC PINS
@@ -97,28 +100,26 @@
 #ifdef PNEUMATICS
     #define PNEUMATIC_PUMP_PIN  7   // Pump driver
     #define PNEUMATIC_PIN       5   // A5 Input analog tank pressure reading
-    #define REGULATOR_PIN 3         // A3 Input analog output pressure reading
+    #define REGULATOR_PIN       3   // A3 Input analog output pressure reading
     #define PNEUMATIC_CART2_PIN 4   // A4 Cartridge 2 analog pressure reading
-    #define HEATER_1_PIN -1         // Heat_1 Output Used for Pneumatics
-    #define TEMP_1_PIN -1           // (Analog Pin Numbering)
-#else
-    #define HEATER_1_PIN 7
-    #define TEMP_1_PIN 1            // A1 (Analog Pin Numbering)
+//     #define HEATER_1_PIN        -1  // Heat_1 Output Used for Pneumatics
+// #else
+//     #define HEATER_1_PIN        7
 #endif
 
-#define TEMP_2_PIN -1
+#define TEMP_2_PIN              -1
 
-#define E0_STEP_PIN         34
-#define E0_DIR_PIN          43
-#define E0_ENABLE_PIN       26
-#define E0_MS1_PIN          65
-#define E0_MS2_PIN          66
+#define E0_STEP_PIN             34
+#define E0_DIR_PIN              43
+#define E0_ENABLE_PIN           26
+#define E0_MS1_PIN              65
+#define E0_MS2_PIN              66
 
-#define E1_STEP_PIN         33
-#define E1_DIR_PIN          42
-#define E1_ENABLE_PIN       25
-#define E1_MS1_PIN          63
-#define E1_MS2_PIN          64
+#define E1_STEP_PIN             33
+#define E1_DIR_PIN              42
+#define E1_ENABLE_PIN           25
+#define E1_MS1_PIN              63
+#define E1_MS2_PIN              64
 
 /**********************************************************
   Fan Pins
@@ -126,25 +127,25 @@
   Fan_1 6
   Fan_2 2
 ***********************************************************/
-#define LED_PIN             13
-#define FAN_PIN             CART1_SIG3_PIN  // FFF fan toggle
-#define FAN_CHASSIS_TOP_PIN 13   // Shared with LED pin for now
-#define FAN_CHASSIS_BOT_PIN 44
+#define LED_PIN                 13
+#define FAN_PIN                 CART1_SIG3_PIN  // FFF fan toggle
+#define FAN_CHASSIS_TOP_PIN     13  // Shared with LED pin for now
+#define FAN_CHASSIS_BOT_PIN     44
 
 /*************************
 	  ADMINISTRATIVE PINS
 *************************/
-#define V_MONITOR_PIN     79  //! Monitor voltage to print engine
-#define PS_ON_PIN         4
-#define KILL_PIN          -1  //80 with Smart Controller LCD
-#define SUICIDE_PIN       -1  //PIN that has to be turned on right after start, to keep power flowing.
-#define SDPOWER           -1
-#define SDSS              53
+#define V_MONITOR_PIN           79  //! Monitor voltage to print engine
+#define PS_ON_PIN               4
+#define KILL_PIN                -1  //80 with Smart Controller LCD
+#define SUICIDE_PIN             -1  //PIN that has to be turned on right after start, to keep power flowing.
+#define SDPOWER                 -1
+#define SDSS                    53
 
 /*************************
 	  FILAMENT SENSE PINS
 *************************/
 #ifdef FILAMENT_SENSOR
   //Filip added pin for Filament sensor analog input 
-  #define FILMNT_SENS_PIN 80  //! Monitor filament low
+  #define FILMNT_SENS_PIN       80  //! Monitor filament low
 #endif
