6d5a335a0a2685265020d3e83f80568c4778f22f
Kevin Murphy <murphyk3@wit.edu>
2015-10-08 18:21:52 -0400

    Make script Cygwin friendly

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 HERE=$(pwd)
-sh $HERE/version.sh . Marlin/_Version.h
+sh ./version.sh . Marlin/_Version.h
 case "$(uname -s)"
   in Darwin)
     ARDUINO_EXEC="~/Applications/Arduino.app"
     ARDUINO_DEP="~/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/"
   ;; Linux)
     echo 'Linux'
-  ;; CYGWIN*|MINGW32*|MSYS*)
+  ;; CYGWIN*)
+    CYGHERE="$(cygpath -aw $(pwd))"
+    ARDUINO_EXEC="C:/Program\ Files\ \(x86\)/Arduino/arduino_debug.exe --verify \"$CYGHERE/Marlin/Marlin.ino\" --pref build.path=$HERE/build/ --pref board=rambo --port $1"
+    ARDUINO_DEP="C:/Program Files (x86)/Arduino/hardware/arduino/avr"
+  ;;MINGW32*|MSYS*)
     ARDUINO_EXEC="C:/Program\ Files\ \(x86\)/Arduino/arduino_debug.exe --verify $HERE/Marlin/Marlin.ino --pref build.path=$HERE/build/ --pref board=rambo --port $1"
     ARDUINO_DEP="C:/Program Files (x86)/Arduino/hardware/arduino/avr"
   ;; *)
     echo 'This operating system is unfamiliar'
-  ;;
 esac
 # Prepare for build by copying in RAMBo boards.txt and pins Files
 
