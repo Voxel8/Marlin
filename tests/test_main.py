4f79e40ec1260e52a8d1cf40a0a14ff1713c4420
Kevin Murphy <murphyk3@wit.edu>
2015-10-14 18:14:59 -0400

    Adjust test file to avoid merge conflicts

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,9 @@
 #! /usr/bin/env python
 
 import unittest
-import argparse
 import numpy as np
 from mecode import G
 
-parser = argparse.ArgumentParser(description='Voxel8 Test Suite')
-parser.add_argument("com_port", help="Device port value",
-                    metavar="PORT")
-args = parser.parse_args()
-
-if not args.com_port:
-    parser.error('Port not given')
 
 g = G(
     print_lines=False,
@@ -19,9 +11,8 @@ g = G(
     direct_write=True,
     direct_write_mode='serial',
     #printer_port="/dev/tty.usbmodem1411",
-    printer_port=args.com_port,
+    printer_port="COM5",
 )
-
 
 def read_profilometer(samples=1):
     samples = np.floor(np.log2(samples))
@@ -36,7 +27,7 @@ def read_profilometer(samples=1):
 class MarlinTestCase(unittest.TestCase):
 
     def test_M237(self):
-        cycles = 10
+        cycles = 2
         measurement_locations = [
             (25, 60),
             (95, 175),
@@ -51,12 +42,29 @@ class MarlinTestCase(unittest.TestCase):
             for j, location in enumerate(measurement_locations):
                 print ".",
                 g.abs_move(*location)
-                g.write('G4 P500')
+                g.write('G4 P300')
                 measurements[j] = read_profilometer(samples=4)
             stdev = np.std(measurements)
-            msg = "Bed level standard deviation was larger than 15 microns"
+            msg = 'Bed level standard deviation was larger than 15 microns'
             self.assertLess(stdev, 15, msg)
 
+    def test_M218(self):
+        # Assert offset echoed correctly
+        g.write('G92 X0 Y0 Z0')
+        g.write('M218 T1 X10 Y10 Z10')
+        response = g.write('M218', resp_needed=True)
+        self.assertIn('0.00,0.00,0.00 10.00,10.00,10.00', response)
+
+        # Assert offset applied to current position on tool change
+        response = g.write('M114', resp_needed=True)
+        self.assertIn('X:0.00 Y:0.00 Z:0.00', response)
+        g.write('T1')
+        response = g.write('M114', resp_needed=True)
+        self.assertIn('X:10.00 Y:10.00 Z:10.00', response)
+
+        # Reset Offset Values and state
+        g.write('M218 T1 X0 Y0 Z0')
+        g.write('T0')
 
 if __name__ == '__main__':
     unittest.main()
