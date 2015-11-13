cef1557ead2ef2296520c5a284f061ceceb709a9
Ricky Rininger <pizzyflavin@gmail.com>
2015-11-13 16:16:15 -0500

    Check for disabled pneum once instead of mult times in SanityCheck.h because dual pneum doesn't require temp/heaters

--- Marlin/SanityCheck.h
+++ Marlin/SanityCheck.h
@@ -280,60 +280,63 @@
   /**
    * Test Heater, Temp Sensor, and Extruder Pins; Sensor Type must also be set.
    */
-  #if EXTRUDERS > 3
-    #if !HAS_HEATER_3
-      #error HEATER_3_PIN not defined for this board.
-    #elif !PIN_EXISTS(TEMP_3)
-      #error TEMP_3_PIN not defined for this board.
-    #elif !PIN_EXISTS(E3_STEP) || !PIN_EXISTS(E3_DIR) || !PIN_EXISTS(E3_ENABLE)
-      #error E3_STEP_PIN, E3_DIR_PIN, or E3_ENABLE_PIN not defined for this board.
-    #elif TEMP_SENSOR_3 == 0
-      #error TEMP_SENSOR_3 is required with 4 EXTRUDERS.
-    #endif
-  #elif EXTRUDERS > 2
-    #if !HAS_HEATER_2
-      #error HEATER_2_PIN not defined for this board.
-    #elif !PIN_EXISTS(TEMP_2)
-      #error TEMP_2_PIN not defined for this board.
-    #elif !PIN_EXISTS(E2_STEP) || !PIN_EXISTS(E2_DIR) || !PIN_EXISTS(E2_ENABLE)
-      #error E2_STEP_PIN, E2_DIR_PIN, or E2_ENABLE_PIN not defined for this board.
-    #elif TEMP_SENSOR_2 == 0
-      #error TEMP_SENSOR_2 is required with 3 or more EXTRUDERS.
-    #endif
-  #elif EXTRUDERS > 1
-    #if !PIN_EXISTS(TEMP_1)
-      #error TEMP_1_PIN not defined for this board.
-    #elif !PIN_EXISTS(E1_STEP) || !PIN_EXISTS(E1_DIR) || !PIN_EXISTS(E1_ENABLE)
-      #error E1_STEP_PIN, E1_DIR_PIN, or E1_ENABLE_PIN not defined for this board.
-    #endif
-  #endif
 
-  #if EXTRUDERS > 1 || ENABLED(HEATERS_PARALLEL)
-    // Pneumatics do not require a heater
-    #if (!HAS_HEATER_1 && DISABLED(PNEUMATICS))
-      #error HEATER_1_PIN not defined for this board.
+  // (Dual) Pneumatics do not require temp sensors/heaters
+  #if DISABLED(PNEUMATICS)
+
+    #if EXTRUDERS > 3
+      #if !HAS_HEATER_3
+        #error HEATER_3_PIN not defined for this board.
+      #elif !PIN_EXISTS(TEMP_3)
+        #error TEMP_3_PIN not defined for this board.
+      #elif !PIN_EXISTS(E3_STEP) || !PIN_EXISTS(E3_DIR) || !PIN_EXISTS(E3_ENABLE)
+        #error E3_STEP_PIN, E3_DIR_PIN, or E3_ENABLE_PIN not defined for this board.
+      #elif TEMP_SENSOR_3 == 0
+        #error TEMP_SENSOR_3 is required with 4 EXTRUDERS.
+      #endif
+    #elif EXTRUDERS > 2
+      #if !HAS_HEATER_2
+        #error HEATER_2_PIN not defined for this board.
+      #elif !PIN_EXISTS(TEMP_2)
+        #error TEMP_2_PIN not defined for this board.
+      #elif !PIN_EXISTS(E2_STEP) || !PIN_EXISTS(E2_DIR) || !PIN_EXISTS(E2_ENABLE)
+        #error E2_STEP_PIN, E2_DIR_PIN, or E2_ENABLE_PIN not defined for this board.
+      #elif TEMP_SENSOR_2 == 0
+        #error TEMP_SENSOR_2 is required with 3 or more EXTRUDERS.
+      #endif
+    #elif EXTRUDERS > 1
+      #if !PIN_EXISTS(TEMP_1)
+        #error TEMP_1_PIN not defined for this board.
+      #elif !PIN_EXISTS(E1_STEP) || !PIN_EXISTS(E1_DIR) || !PIN_EXISTS(E1_ENABLE)
+        #error E1_STEP_PIN, E1_DIR_PIN, or E1_ENABLE_PIN not defined for this board.
+      #endif
     #endif
-  #endif
 
-  #if TEMP_SENSOR_1 == 0
-   // Pneumatics do not require a temp sensor
-    #if (EXTRUDERS > 1 && DISABLED(PNEUMATICS))
-      #error TEMP_SENSOR_1 is required with 2 or more EXTRUDERS.
-    #elif ENABLED(TEMP_SENSOR_1_AS_REDUNDANT)
-      #error TEMP_SENSOR_1 is required with TEMP_SENSOR_1_AS_REDUNDANT.
+    #if EXTRUDERS > 1 || ENABLED(HEATERS_PARALLEL)
+      #if !HAS_HEATER_1
+        #error HEATER_1_PIN not defined for this board.
+      #endif
     #endif
-  #endif
 
-  #if !HAS_HEATER_0
-    #error HEATER_0_PIN not defined for this board.
-  #elif !PIN_EXISTS(TEMP_0)
-    #error TEMP_0_PIN not defined for this board.
-  #elif !PIN_EXISTS(E0_STEP) || !PIN_EXISTS(E0_DIR) || !PIN_EXISTS(E0_ENABLE)
-    #error E0_STEP_PIN, E0_DIR_PIN, or E0_ENABLE_PIN not defined for this board.
-  #elif TEMP_SENSOR_0 == 0
-    #error TEMP_SENSOR_0 is required.
-  #endif
+    #if TEMP_SENSOR_1 == 0
+      #if EXTRUDERS > 1
+        #error TEMP_SENSOR_1 is required with 2 or more EXTRUDERS.
+      #elif ENABLED(TEMP_SENSOR_1_AS_REDUNDANT)
+        #error TEMP_SENSOR_1 is required with TEMP_SENSOR_1_AS_REDUNDANT.
+      #endif
+    #endif
 
+    #if !HAS_HEATER_0
+      #error HEATER_0_PIN not defined for this board.
+    #elif !PIN_EXISTS(TEMP_0)
+      #error TEMP_0_PIN not defined for this board.
+    #elif !PIN_EXISTS(E0_STEP) || !PIN_EXISTS(E0_DIR) || !PIN_EXISTS(E0_ENABLE)
+      #error E0_STEP_PIN, E0_DIR_PIN, or E0_ENABLE_PIN not defined for this board.
+    #elif TEMP_SENSOR_0 == 0
+      #error TEMP_SENSOR_0 is required.
+    #endif
+
+  #endif // DISABLED(PNEUMATICS)
   /**
    * Warnings for old configurations
    */
