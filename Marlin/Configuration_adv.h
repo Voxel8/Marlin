5a187480e6443cdd7460beac315d0a7a11e7f4c4
kdumontnu <kyle@voxel8.co>
2015-12-07 14:27:04 -0500

    Better math for +24V_SW test limits

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -308,9 +308,9 @@ Wv = (VRef / 1.66) * 255
   
 // Power supply enable
 // defines the voltage above which there is determined no short circuit
-#define PS_ENABLE_LOWER_LIMIT 205  // 2^10 * ([Rl/(499 + Rl)] + 200mV/5V), Rl = 100 Ohm
+#define PS_ENABLE_LOWER_LIMIT 220  // 2^10 * ([Rl/(499 + Rl)] + 250mV/5V), Rl = 100 Ohm
 // defines the voltage below which there is determined no open circuit or short to 5V line
-#define PS_ENABLE_UPPER_LIMIT 930  // 2^10 * ([Rl/(499 + Rl)] + 200mV/5V), Rl = 5k
+#define PS_ENABLE_UPPER_LIMIT 980  // 2^10 * ([Rl/(499 + Rl)] + 250mV/5V), Rl = 5k
 
 //===========================================================================
 //=============================Additional Features===========================
