16e869bb9b2c1bb74b9dd8d02c6260038c03f976
Dan Thompson <danthompson41@gmail.com>
2016-04-04 13:50:55 -0400

    Initial commit, untested. Can disable with MCODE or define currently

--- Marlin/HeatedBed.cpp
+++ Marlin/HeatedBed.cpp
@@ -20,12 +20,13 @@
 //============================ Private Variables ============================
 //===========================================================================
 
+static bool bedRemovalCheckEnabled = 1;
+
 //===========================================================================
 //====================== Private Functions Prototypes =======================
 //===========================================================================
 
 void heatedBedRemovedError(void);
-
 //===========================================================================
 //============================ Public Functions =============================
 //===========================================================================
@@ -35,7 +36,7 @@ void heatedBedRemovedError(void);
  * @returns    Returns true if the heated bed is present
  */
 bool HeatedBed__PresentCheck(void) {
-	#if ENABLED(HEATED_BED_PRESENT_CHECK)
+	if (bedRemovalCheckEnabled && HEATED_BED_PRESENT_CHECK) {
 		bool returnValue = false;
 		if (READ(BED_AVAIL_PIN) == HIGH) {
 			returnValue = true;
@@ -44,9 +45,29 @@ bool HeatedBed__PresentCheck(void) {
 			heatedBedRemovedError();
 		}
 		return returnValue;
-	#else
+	}
+	else {
 		return true;
-	#endif
+	}
+}
+
+void HeatedBed__SetPresentCheck(bool value) {
+	if (value == true) {
+		SERIAL_PROTOCOL("Heated Bed Check Enabled")
+		SERIAL_EOL
+		bedRemovalCheckEnabled = 1
+	}
+	else if (value == false) {
+		SERIAL_PROTOCOL("Heated Bed Check Disabled")
+		SERIAL_EOL;
+		bedRemovalCheckEnabled = 0
+	}
+	else {
+		SERIAL_PROTOCOL("Invalid value for Heated Bed Check Set")
+		SERIAL_EOL;
+		return;
+	}
+	bedRemovalCheckEnabled = value;
 }
 
 //===========================================================================
