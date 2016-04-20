23c470597d9e236826affe18855f21f062941700
Dan Thompson <danthompson41@gmail.com>
2016-04-20 14:47:01 -0400

    Moved GCode.pp to GCode.h so that the functions could be inlined

--- Marlin/GCodes.h
+++ Marlin/GCodes.h
@@ -1,18 +1,47 @@
 /**
- * GCodes.h - Mcode Interface for GCodes.cpp
+ * GCodes.h - Contains codes for Gcodes.
  * Author: Dan Thompson (danthompson41@gmail.com)
  * Date: 4/8/2016
- * Used as an interface for Gcodes, to decouple them from Marlin_main.
+ * Used as the home for Gcodes, to decouple them from Marlin_main. 
+ * Note on the weirdness of having the definitions in an h file: This is to
+ * facilitate the inline functions. Inline is only file specific, so doesn't
+ * work if you count on the linker (ex, using the normal .cpp with an h file
+ * interface). Directly including the code in an H file is the easiest way to
+ * do this.
  * Copyright (C) 2016 Voxel8
  */
 
 #ifndef G_CODES_H_
 #define G_CODES_H_
 
+#include "Marlin.h"
+#include "GCodeUtility.h"
+#include "planner.h"
+
 //===========================================================================
 //============================= Public Functions ============================
 //===========================================================================
 
-void gcode_M272(void);
+inline void gcode_M272(void) {
+  for (int8_t i = 0; i < NUM_AXIS; i++) {
+    if (code_seen(axis_codes[i])) {
+      if (i == E_AXIS) {
+        float value = (213.3333333 / (1 + (code_value() / 1000)));
+        if (value < 20.0) {
+          float factor =
+              axis_steps_per_unit[i] /
+              value;  // increase e constants if M92 E14 is given for netfab.
+          max_e_jerk *= factor;
+          max_feedrate[i] *= factor;
+          axis_steps_per_sqr_second[i] *= factor;
+        }
+        axis_steps_per_unit[i] = value;
+      } else {
+        float value = (213.3333333 / (1 + (code_value() / 1000)));
+        axis_steps_per_unit[i] = value;
+      }
+    }
+  }
+}
 
 #endif  // G_CODES_H_
