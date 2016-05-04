04900d3dec3c5be40375fff66b7a1d54acb0a960
kdumontnu <kyle@voxel8.co>
2016-05-04 19:08:46 -0400

    Initial Setting for Metalworks Electropneumatic regulator

--- Marlin/Regulator.h
+++ Marlin/Regulator.h
@@ -8,14 +8,14 @@
 /*================================================================================*/
 // Calculation for BITS_PER_PSI:
 // R1 = 499
-// R2 (input impedance of sensor) = 10k
+// R2 (input impedance of sensor) = 168k
 // n = 12
 // BITS_PER_PSI = (2^n/OUTPUT_PSI_MAX) / (R2/(R1 + R2)) approx = 33.1
 
-#define BITS_PER_PSI    33.1
+#define BITS_PER_PSI    41.08
 
 #define REG_OFFSET      0.5 // psi
-#define REG_HYSTERESIS  0.2 // psi
+#define REG_HYSTERESIS  0.0 // psi
 #define MCP_CONST       (REG_OFFSET - (REG_HYSTERESIS/2.0))
 #define REGULATOR_LOW_P 2
 
--- tests/test_i2c.py
+++ tests/test_i2c.py
@@ -5,6 +5,33 @@ import numpy as np
 from mecode import G
 from time import sleep
 import re
+from colorlog import ColoredFormatter
+import logging
+
+logging.basicConfig(level=logging.DEBUG,  # DEBUG, INFO, WARNING
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%d-%m-%y %H:%M:%S',
+                    filename='console.log',
+                    filemode='w'
+                    )
+
+console = logging.StreamHandler()
+console.setLevel(logging.ERROR)
+formatter = ColoredFormatter(
+    "%(black,bg_white)s%(asctime)s%(reset)s %(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s",
+    datefmt='%d-%m-%y %H:%M:%S',
+    reset=True,
+    log_colors={
+        'DEBUG':    'cyan',
+        'INFO':     'green',
+        'WARNING':  'yellow',
+        'ERROR':    'red',
+        'CRITICAL': 'white,bg_red',
+    }
+)
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
 
 class I2CTestCase(unittest.TestCase):
 
@@ -14,8 +41,8 @@ class I2CTestCase(unittest.TestCase):
             aerotech_include=False,
             direct_write=True,
             direct_write_mode='serial',
-            #printer_port="/dev/tty.usbmodem1421",
-            printer_port="COM5",
+            # printer_port="/dev/tty.usbmodem1421",
+            printer_port="COM21",
         )
 
     def tearDown(self):
@@ -25,28 +52,90 @@ class I2CTestCase(unittest.TestCase):
         self.g.teardown()
         self.g = None
 
-    def commandparser(self,command, p1 = '', p2 = '', p3=''):
-    	g = self.g
-    	resp = g.write(command + ' ' + p1+ ' ' + p2 + ' ' + p3, resp_needed=True);
-    	print(str(command + ' ' + p1+ ' ' + p2 + ' ' + p3))
-    	testsplit = re.findall('Packet',resp)
-    	if len(testsplit):
-    		print("Dropped " + str(len(testsplit)) + " Packets from address " + str(p1))
-    	testsplit = re.findall('Timeout',resp)
-    	if len(testsplit):
-    		print(str(len(testsplit)) + " Timeouts from " + str(p1))
+    def commandparser(self, command, p1='', p2='', p3=''):
+        g = self.g
+        resp = g.write(
+            command + ' ' + p1 + ' ' + p2 + ' ' + p3, resp_needed=True)
+        logging.debug(str(command + ' ' + p1 + ' ' + p2 + ' ' + p3 +'  Response: ' + resp))
+        PacketSplit = re.findall('Packet', resp)
+        TimeoutSplit = re.findall('Timeout', resp)
+        if len(PacketSplit):
+            logging.error(
+                "Dropped " + str(len(PacketSplit)) + " Packets from address " + str(p1) + ". " + str(p1) + " is either not present or not communicating.")
+            resp = 0
+            self.assertEqual(len(PacketSplit), 0, "Dropped packets, target is either not connected or not programmed")
+        if len(TimeoutSplit):
+            logging.error(str(len(TimeoutSplit)) + " Timeouts from " + str(p1))
+            resp = 0
+            self.assertEqual(len(TimeoutSplit), 0, "Timed out I2C, one of the peripherals is likely holding the data line low")
+        return resp
+
+    def parseCartridgeStatus(self, cartridgeStatus, cartridgeNumber):
+        logging.info("%s Info", cartridgeNumber)
+        infoList = re.split('\W+', cartridgeStatus)
+        logging.debug(infoList)
+        logging.info("-------------------------------------")
+        logging.info("Cartridge Serial Number: %s", infoList[2])
+        logging.info("Cartridge Firmware Version: %s", infoList[18])
+        logging.info("Cartridge Programmer Station: %s", infoList[5])
+        logging.info("Cartridge Type: %s", infoList[8])
+        logging.info("Cartridge Size: %s", infoList[11])
+        logging.info("Cartridge Material: %s", infoList[14])
+
+        if (int(infoList[18]) == 255):
+            logging.error(
+                "%s FLASH not loaded properly, or cartridge is out of date", cartridgeNumber)
+            logging.info(' ')
+            self.assertNotEqual(int(infoList[18]), 255,"Cartridge FLASH not loaded properly, or cartridge is out of date")
+
+        if (int(infoList[8]) == 15) and (cartridgeNumber != "Cartridge Holder"):
+            logging.error("%s EEPROM not loaded properly", cartridgeNumber)
+            logging.info(' ')
+            self.assertNotEqual(int(infoList[8]), 15, "Cartridge EEPROM not loaded properly")
+        logging.info(' ')
     ###########################################################################
     #
     #                               Tests
     #
     ###########################################################################
 
-    def test_i2c(self):
-      	self.commandparser('M245','C0')
-      	self.commandparser('M245','C1')
-      	for i in range(0,30):
-      		string = 'E'+str(i)
-      		self.commandparser('M244','C0',string)
-      		self.commandparser('M244','C1',string)
+    def test_i2c_alltargets(self):
+        logging.info("Testing all I2C Targets")
+        for i in range(0, 30):
+            string = 'E'+str(i)
+            self.commandparser('M244', 'C0', string)
+            self.commandparser('M244', 'C1', string)
+            self.commandparser('M244', 'C2', string)
+
+    def test_i2c_cart0(self):
+        logging.info("Testing Cartridge 0 I2C")
+        for i in range(0, 30):
+            string = 'E'+str(i)
+            self.commandparser('M244', 'C0', string)
+
+    def test_i2c_cart1(self):
+        logging.info("Testing Cartridge 1 I2C")
+        for i in range(0, 30):
+            string = 'E'+str(i)
+            self.commandparser('M244', 'C1', string)
+
+    def test_i2c_cartridgeHolder(self):
+        logging.info("Testing Cartridge Holder I2C")
+        for i in range(0, 30):
+            string = 'E'+str(i)
+            self.commandparser('M244', 'C2', string)
+
+    def test_cart1_info(self):
+        cart0status = self.commandparser('M245', 'C0')
+        self.parseCartridgeStatus(cart0status, "Cartridge 0")
+
+    def test_cart2_info(self):
+        cart1status = self.commandparser('M245', 'C1')
+        self.parseCartridgeStatus(cart1status, "Cartridge 1")
+
+    def test_cartridgeholder(self):
+        cartridgeHolderStatus = self.commandparser('M245', 'C2')
+        self.parseCartridgeStatus(cartridgeHolderStatus, "Cartridge Holder")
+
 if __name__ == '__main__':
     unittest.main()
