73cf90c2ae1a85a5f3f36a80ba3857d1713414a6
Sean Troiano <sean@voxel8.com>
2016-09-27 15:27:59 -0400

    Add auger handling for pressure multiplier
    For a linear response we typically want auger pressure to remain constant
    while adjusting the extrusion multiplier, so this toggles that behavior
    automatically... can still be enabled/disabled at will with M222.

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -260,6 +260,7 @@ inline void refresh_cmd_timeout() { previous_cmd_ms = millis(); }
 extern bool axis_relative_modes[];
 extern int feedrate_multiplier;
 extern bool volumetric_enabled;
+extern bool pressure_multiplier;
 extern int extruder_multiplier[EXTRUDERS]; // sets extrude multiply factor (in percent) for each extruder individually
 extern float filament_size[EXTRUDERS]; // cross-sectional area of filament (in millimeters), typically around 1.75 or 2.85, 0 disables the volumetric calculations for the extruder.
 extern float volumetric_multiplier[EXTRUDERS]; // reciprocal of cross-sectional area of filament (in square millimeters), stored this way to reduce computational burden in planner
